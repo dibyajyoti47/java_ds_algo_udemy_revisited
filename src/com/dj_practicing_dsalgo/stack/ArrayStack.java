3337a924f72cd46af480159cb78f04a054bb8961
dibyajyoti_behera <dibyajyoti_behera@dellteam.com>
2020-12-29 08:51:44 +0530

    stack completed

--- src/com/dj_practicing_dsalgo/stack/ArrayStack.java
+++ src/com/dj_practicing_dsalgo/stack/ArrayStack.java
@@ -1,12 +1,50 @@
 package com.dj_practicing_dsalgo.stack;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
 public class ArrayStack<T> {
 
     private Object[] stack;
+    private int size;
+    private int pointer = 0;
 
     public ArrayStack (int capacity) {
         this.stack = new Object[capacity];
     }
 
+    public void push (T element){
+        if (size == stack.length) ensureCapacity();
+        stack[pointer++] = element;
+        size++;
+    }
+
+    public T pop () {
+        if (size == 0) throw new EmptyStackException();
+        T removedElement = (T) stack[--pointer];
+        stack[pointer] = null;
+        size--;
+        return removedElement;
+    }
+
+    public void ensureCapacity() {
+        Object[] newStack = new Object[size*2];
+        System.arraycopy(stack, 0, newStack, 0, size);
+        stack = newStack;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void printFullStack () {
+        Arrays.stream(stack).forEach(System.out::println);
+    }
+
+    public void printStack () {
+        for (int i=0; i<size; i++){
+            System.out.println(stack[i]);
+        }
+    }
 }
--- src/com/dj_practicing_dsalgo/stack/Main.java
+++ src/com/dj_practicing_dsalgo/stack/Main.java
@@ -2,6 +2,20 @@ package com.dj_practicing_dsalgo.stack;
 
 public class Main {
     public static void main(String[] args) {
-
+        ArrayStack<String> stack = new ArrayStack<String>(3);
+        stack.push("Linku");
+        stack.push("Tinku");
+        stack.push("Pinku");
+        stack.push("Chinku");
+        stack.printStack();
+        System.out.println(stack.getSize());
+        System.out.println("-------------------------------");
+        System.out.println(stack.pop());
+        System.out.println(stack.getSize());
+        stack.printFullStack();
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop()); //throws exception
     }
 }
